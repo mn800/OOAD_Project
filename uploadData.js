5ec54560991c313b99d72744ba4dd59d3bd8061f
RoseQuartz08 <71607278+RoseQuartz08@users.noreply.github.com>
2020-11-25 18:43:53 -0500

    Add files via upload

---
+++ uploadData.js
@@ -0,0 +1,23 @@
+const url  = 'name.php'
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+	e.prevemtDefault()
+	
+	const files = document.querySelector('[type=file]').files
+	const formData = new FormData()
+	
+	for (let i = 0; i < files.length; i++){
+		let file = files[i]
+		
+		formData.append('files[]', file)
+	}
+	
+	fetch(url, {
+		method: 'POST',
+		body: formData,
+		
+	}).then((response) => {
+		console.log(response)
+	})
+})
